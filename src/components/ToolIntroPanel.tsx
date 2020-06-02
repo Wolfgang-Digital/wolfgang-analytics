403e146921fc0947946f33574963988275b4198a
Fionn Casey <fionn.casey@ad.wolfgangdigital.com>
2020-06-02 13:56:49 +0100

    Removed hover anim

--- src/components/ToolIntroPanel.tsx
+++ src/components/ToolIntroPanel.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image, Heading, Text, PseudoBox, Button, Link as ExternalLink, Flex } from '@chakra-ui/core';
+import { Grid, Image, Heading, Text, Button, Link as ExternalLink, Flex } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
 
 interface Props {
@@ -13,45 +13,39 @@ interface Props {
 
 const ToolIntroPanel: React.FC<Props> = ({ title, image, description, link, learnMoreLink, colour }) => {
   return (
-    <PseudoBox
-      _hover={{
-        animation: 'slideUp 300ms ease-in-out forwards'
-      }}
+    <Grid
+      templateColumns="3fr 1fr"
+      templateRows="auto 1fr"
+      backgroundColor={colour}
+      color="white"
+      padding={4}
+      borderRadius={4}
+      boxShadow="rgba(0, 0, 0, 0.2) 1px 2px 2px 0px"
     >
-      <Grid
-        templateColumns="3fr 1fr"
-        templateRows="auto 1fr"
-        backgroundColor={colour}
-        color="white"
-        padding={4}
-        borderRadius={4}
-        boxShadow="rgba(0, 0, 0, 0.2) 1px 2px 2px 0px"
-      >
-        <Heading as="h2" size="lg" marginTop="auto">
-          {title}
-        </Heading>
-        <Image src={image} size="42px" marginLeft="auto" objectFit="cover" alt={title} />
-        <>
-          <Text marginTop={2} gridColumn="span 2">
-            {description}
-          </Text>
-          <Flex marginTop={4}>
-            {learnMoreLink && (
-              <ExternalLink href={learnMoreLink} isExternal>
-                <Button variant="outline" color="white" _hover={{ color: 'white' }} rightIcon="external-link" size="sm" marginRight={2}>
-                  Learn More
-                </Button>
-              </ExternalLink>
-            )}
-            <Link to={link}>
-              <Button variant="outline" color="white" _hover={{ color: 'white' }} rightIcon="link" size="sm">
-                Go to Tool
+      <Heading as="h2" size="lg" marginTop="auto">
+        {title}
+      </Heading>
+      <Image src={image} size="42px" marginLeft="auto" objectFit="cover" alt={title} />
+      <>
+        <Text marginTop={2} gridColumn="span 2">
+          {description}
+        </Text>
+        <Flex marginTop={4}>
+          {learnMoreLink && (
+            <ExternalLink href={learnMoreLink} isExternal>
+              <Button variant="outline" rightIcon="external-link" size="sm" marginRight={2}>
+                Learn More
               </Button>
-            </Link>
-          </Flex>
-        </>
-      </Grid>
-    </PseudoBox>
+            </ExternalLink>
+          )}
+          <Link to={link}>
+            <Button variant="outline" rightIcon="link" size="sm">
+              Go to Tool
+            </Button>
+          </Link>
+        </Flex>
+      </>
+    </Grid>
   );
 };
 
